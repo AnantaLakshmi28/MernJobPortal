b86b31831c3584f21009a34b83d821a4f72403bd
AnantaLakshmi28 <sabbellaanantalakshmi@gmail.com>
2025-11-09 11:45:11 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','https://mern-job-portal-cyan.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
